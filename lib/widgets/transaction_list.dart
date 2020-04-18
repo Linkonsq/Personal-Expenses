8536495ca1e6213891562b927c1259ea5fab4fb4
Linkon Siddique <lsiddique415@gmail.com>
2020-04-19 05:33:49 +0600

    Working with theme and custom fonts

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -20,7 +20,7 @@ class TransactionList extends StatelessWidget {
                   child: Text(
                     '\$${transactions[index].amount.toStringAsFixed(2)}',
                     style: TextStyle(
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
@@ -31,7 +31,7 @@ class TransactionList extends StatelessWidget {
                   ),
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                       width: 2,
                     ),
                   ),
@@ -42,10 +42,7 @@ class TransactionList extends StatelessWidget {
                   children: <Widget>[
                     Text(
                       transactions[index].title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      style: Theme.of(context).textTheme.title,
                     ),
                     Text(
                       DateFormat('dd.MM.yyyy').format(transactions[index].date),
